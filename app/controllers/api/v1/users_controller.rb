f3e8b3246ae4223d82a00b8812bdfcde5b48d8bf
deedeeh <dinahafez.8@gmail.com>
2018-12-13 13:31:47 +0000

    created routes and added users controller methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::UsersController < ApplicationController
+
+    def show 
+        @user = User.find(params[:id])
+        if @user 
+            render json: @user
+        else
+            render json: {error: 'User was not found'}, status: 404
+        end
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            render json: @user
+        else
+            render json: {error: 'Unable to create this user'}, status: 400
+        end
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :name, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show, :create]
+      resources :questionnaires, only: [:show, :create, :update]
+      resources :tasks, only: [:show, :create] 
+    end
+  end
 end
