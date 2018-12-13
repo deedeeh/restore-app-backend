b03ac7af1149fc53581862f0676c2f7b764d91b3
deedeeh <dinahafez.8@gmail.com>
2018-12-13 13:54:33 +0000

    created the controllers methods for questionnaires and tasks

--- app/controllers/api/v1/questionnaires_controller.rb
+++ app/controllers/api/v1/questionnaires_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::QuestionnairesController < ApplicationController
+    before_action :find_questionnaire, only: [:show, :update]
+
+    def show 
+        if @questionnaire 
+            render json: @questionnaire
+        else
+            render json: {error: 'Questionnaire was not found'}, status: 404
+        end
+    end
+
+    def create 
+        @questionnaire = Questionnaire.new(questionnaire_params)
+        if @questionnaire.save 
+            render json: @questionnaire
+        else
+            render json: {error: 'Unable to create this questionnaire'}, status: 400
+        end
+    end 
+
+    def update 
+        @questionnaire.update(questionnaire_params)
+        if @questionnaire.save 
+            render json: @questionnaire, status: 202
+        else
+            render json: {errors: @questionnaire.errors.full_messages}, status: 422
+        end
+    end
+
+    private 
+
+    def questionnaire_params 
+        params.require(:questionnaire).permit(:user_id, :job_title, :work_hours, :breaks, :breaks_quantity, :break_length)
+    end
+
+    def find_questionnaire
+        @questionnaire = Questionnaire.find(params[:id])
+    end
 end
--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::TasksController < ApplicationController
+
+    def show 
+        @task = Task.find(params[:id])
+        if @task 
+            render json: @task
+        else
+            render json: {error: 'Task was not found'}, status: 404
+        end
+    end
+
+    def create 
+        @task = Task.new(task_params)
+        if @task.save 
+            render json: @task
+        else
+            render json: {error: 'Unable to create this task'}, status: 400
+        end
+    end 
+
+    private 
+
+    def task_params 
+        params.require(:task).permit(:user_id, :task_type, :task_quantity, :each_task_length)
+    end
+
 end
