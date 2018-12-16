89400d7887be3d5d1c48fb3b77676bc1c1a1fa71
deedeeh <dinahafez.8@gmail.com>
2018-12-16 14:12:21 +0000

    Fixed a typo in the serializer for questionnaires

--- app/serializers/questionnaire_serializer.rb
+++ app/serializers/questionnaire_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionnaireSerializer < ActiveModel::Serializer
-  attributes :id, :user, :job_title, :working_hours_from, :working_houts_to, :take_breaks, :breaks_quantity, :break_length
+  attributes :id, :user, :job_title, :working_hours_from, :working_hours_to, :take_breaks, :breaks_quantity, :break_length
 
   def user 
     {id: self.object.user.id, 
